ea41f59d060d87a01da5acc494bb4a1ec282caba
Simon Watts <simon@wattmorr.com.au>
2023-04-07 11:43:10 +1000

    Implemented ArrayStack to use in follow_path

--- data_structures/stack_adt.py
+++ data_structures/stack_adt.py
@@ -8,6 +8,7 @@ __docformat__ = 'reStructuredText'
 
 from abc import ABC, abstractmethod
 from typing import TypeVar, Generic
+from data_structures.referential_array import ArrayR
 T = TypeVar('T')
 
 
@@ -48,3 +49,55 @@ class Stack(ABC, Generic[T]):
     def clear(self):
         """ Clears all elements from the stack. """
         self.length = 0
+
+class ArrayStack(Stack[T]):
+    """ Implementation of a stack with arrays.
+
+    Attributes:
+         length (int): number of elements in the stack (inherited)
+         array (ArrayR[T]): array storing the elements of the queue
+
+    ArrayR cannot create empty arrays. So MIN_CAPACITY used to avoid this.
+    """
+    MIN_CAPACITY = 1
+
+    def __init__(self, max_capacity: int) -> None:
+        """ Initialises the length and the array with the given capacity.
+            If max_capacity is 0, the array is created with MIN_CAPACITY.
+        """
+        Stack.__init__(self)
+        self.array = ArrayR(max(self.MIN_CAPACITY, max_capacity))
+
+    def is_full(self) -> bool:
+        """ True if the stack is full and no element can be pushed. """
+        return len(self) == len(self.array)
+
+    def push(self, item: T) -> None:
+        """ Pushes an element to the top of the stack.
+        :pre: stack is not full
+        :raises Exception: if the stack is full
+        """
+        if self.is_full():
+            raise Exception("Stack is full")
+        self.array[len(self)] = item
+        self.length += 1
+
+    def pop(self) -> T:
+        """ Pops the element at the top of the stack.
+        :pre: stack is not empty
+        :raises Exception: if the stack is empty
+        """
+        if self.is_empty():
+            raise Exception("Stack is empty")
+        self.length -= 1
+        return self.array[self.length]
+
+    def peek(self) -> T:
+        """ Returns the element at the top, without popping it from stack.
+        :pre: stack is not empty
+        :raises Exception: if the stack is empty
+        """
+        if self.is_empty():
+            raise Exception("Stack is empty")
+        return self.array[self.length-1]
+
